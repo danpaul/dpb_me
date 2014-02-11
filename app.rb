29aac3a97430a3d52b20dfbe413349fa7d713114
danpaul <pt2323@gmail.com>
2014-02-10 22:59:27 -0500

    ga debugging

--- app.rb
+++ app.rb
@@ -79,7 +79,11 @@ __END__
 			$(window).load(function(){
 				$('#content').fadeIn();
 			});
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		:javascript
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 			ga('create', 'UA-17739609-1', 'dpb.me');
 			ga('send', 'pageview');
 
